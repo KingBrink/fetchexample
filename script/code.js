bc4b2af321f0ca5568ea33527e85a33fa3a27708
KingBrink <kyle.brink1602@gmail.com>
2024-06-04 16:00:14 +0200

    Updatesjoel1

--- script/code.js
+++ script/code.js
@@ -1,35 +1,66 @@
-function fetchData() {
+// function fetchData() {
+//     let container = document.querySelector('[data-container]')
+//     fetch("https://kingbrink.github.io/onlinestore/data/")
+//         .then((res) => {
+//             return res.json()
+//         })
+//         .then((data) => {
+//             data.results.forEach((product) => {
+//                 container.innerHTML += `
+//                     <div class='card'>
+//                         <h2>${product.productName}</h2>
+//                         <h4>
+//                             ${product.category}
+//                         </h4>
+//                         <img src='${product.img_url}' loading='lazy'>
+//                         <p>
+//                             <span>Description: ${product.description}
+//                             </span>
+//                         </p>
+//                         <p>
+//                             <span>Price: R${product.amount}
+//                             </span>
+//                         </p>
+//                         <div class='button-wrapper'>
+//                             <button>Add to cart</button>
+//                         </div>
+//                     </div>
+//                 `
+//             })
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// }
+// fetchData()
+
+async function fetchData() {
     let container = document.querySelector('[data-container]')
-    fetch("https://kingbrink.github.io/onlinestore/data/")
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            data.results.forEach((product) => {
-                container.innerHTML += `
+    let res = await fetch("https://kingbrink.github.io/onlinestore/data/")
+    //Destructuring
+    let {results} = await res.json()
+    results.forEach(product => {
+        container.innerHTML += `
                     <div class='card'>
-                        <h2>${product.productName}</h2>
-                        <h4>
+                         <h2>${product.productName}</h2>
+                         <h4>
                             ${product.category}
                         </h4>
-                        <img src='${product.img_url}' loading='lazy'>
+                         <img src='${product.img_url}' loading='lazy'>
                         <p>
-                            <span>Description: ${product.description}
+                           <span>Description: ${product.description}
                             </span>
-                        </p>
+                         </p>
                         <p>
                             <span>Price: R${product.amount}
                             </span>
                         </p>
-                        <div class='button-wrapper'>
-                            <button>Add to cart</button>
+                         <div class='button-wrapper'>
+                             <button>Add to cart</button>
                         </div>
                     </div>
                 `
-            })
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-}
+             })
+         }
 fetchData()
+
